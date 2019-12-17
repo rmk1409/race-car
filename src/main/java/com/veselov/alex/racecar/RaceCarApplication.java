ebb32b87d733e18b62d6e66bd8bbb84fb80009dc
Pogorelov Roman <rmk140987@gmail.com>
2019-12-17 15:26:47 +0300

    Git init

---
+++ src/main/java/com/veselov/alex/racecar/RaceCarApplication.java
@@ -0,0 +1,13 @@
+package com.veselov.alex.racecar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaceCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaceCarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/veselov/alex/racecar/RaceCarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veselov.alex.racecar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaceCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
