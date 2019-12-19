b8f9daca5abca7f5f259d10f83904a87ff96bf53
Pogorelov Roman <roman.pogorelov@axamit.com>
2019-12-19 08:49:48 +0300

    Refactoring

--- src/main/java/com/veselov/alex/racecar/service/parser/CarsAvByParser.java
+++ src/main/java/com/veselov/alex/racecar/service/parser/CarsAvByParser.java
@@ -1,7 +1,6 @@
 package com.veselov.alex.racecar.service.parser;
 
 import com.veselov.alex.racecar.data.dao.AutoRepository;
-import com.veselov.alex.racecar.data.dao.QueryRepository;
 import com.veselov.alex.racecar.data.entity.Auto;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
@@ -16,41 +15,72 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * It parses wwww.carsav.by site
+ */
 @Service
 public class CarsAvByParser {
     private static final Logger LOGGER = LoggerFactory.getLogger(CarsAvByParser.class);
 
     @Autowired
-    private QueryRepository queryRepository;
-    @Autowired
-    private AutoRepository autoRepository;
+    private AutoRepository repository;
 
-    public List<Auto> handlePagination(String startLink) {
+    /**
+     * It parses query and it finds cars.
+     *
+     * @param startHRef
+     * @return
+     */
+    public List<Auto> parseSite(String startHRef) {
         List<Auto> autos = new ArrayList<>();
-        Connection connection = Jsoup.connect(startLink);
+        Connection connection = Jsoup.connect(startHRef);
         Document document;
         try {
             document = connection.get();
-            int paginationNumber = (int) Math.ceil(Integer.parseInt(document.select("header .heading-title").text().replaceAll("\\D+", "")) / 25.0);
-            for (int i = 1; i <= paginationNumber; i++) {
-                Elements select = document.select(".listing-item");
-                select.forEach(car -> {
-                    String imgSrc = car.select(".listing-item-image img").attr("src");
-                    Elements body = car.select(".listing-item-body");
-                    String name = body.select(".listing-item-title a").text();
-                    String description = body.select(".listing-item-desc").text().substring(6);
-                    String link = body.select(".listing-item-title a").attr("href");
-                    Integer price = Integer.parseInt(body.select(".listing-item-price small").text().replaceAll("\\s", ""));
-                    Integer year = Integer.parseInt(body.select(".listing-item-desc span").text().substring(0, 4));
-                    autos.add(new Auto(imgSrc, name, description, link, price, year));
-                });
-                document = Jsoup.connect(startLink.replace("?", String.format("/page/%d?", i + 1))).get();
-            }
+            this.handlePagination(startHRef, autos, document);
             LOGGER.info("Found {} cars", autos.size());
-            autoRepository.saveAll(autos);
+            repository.saveAll(autos);
         } catch (IOException e) {
             LOGGER.error(e.getMessage(), e);
         }
         return autos;
+    }
+
+    /**
+     * It handles pagination for this query.
+     *
+     * @param startHRef
+     * @param autos
+     * @param document
+     * @throws IOException
+     */
+    private void handlePagination(String startHRef, List<Auto> autos, Document document) throws IOException {
+        int pageQuantity = getPageQuantity(document);
+        for (int i = 1; i <= pageQuantity; i++) {
+            Elements select = document.select(".listing-item");
+            select.forEach(car -> {
+                String imgSrc = car.select(".listing-item-image img").attr("src");
+                Elements body = car.select(".listing-item-body");
+                String name = body.select(".listing-item-title a").text();
+                String description = body.select(".listing-item-desc").text().substring(6);
+                String link = body.select(".listing-item-title a").attr("href");
+                Integer price = Integer.parseInt(body.select(".listing-item-price small").text().replaceAll("\\s", ""));
+                Integer year = Integer.parseInt(body.select(".listing-item-desc span").text().substring(0, 4));
+                autos.add(new Auto(imgSrc, name, description, link, price, year));
+            });
+            document = Jsoup.connect(startHRef.replace("?", String.format("/page/%d?", i + 1))).get();
+        }
+    }
+
+    /**
+     * It gets quantity of pages.
+     *
+     * @param document
+     * @return
+     */
+    private int getPageQuantity(Document document) {
+        final double shownCarsOnOnePage = 25.0;
+        String text = document.select("header .heading-title").text();
+        return (int) Math.ceil(Integer.parseInt(text.replaceAll("\\D+", "")) / shownCarsOnOnePage);
     }
 }
