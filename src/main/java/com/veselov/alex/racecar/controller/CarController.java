315cd48179bbdd0b791b9664e6151dfdfc7478c9
Pogorelov Roman <rmk140987@gmail.com>
2019-12-27 13:50:36 +0300

    Add simple pagination

--- src/main/java/com/veselov/alex/racecar/controller/CarController.java
+++ src/main/java/com/veselov/alex/racecar/controller/CarController.java
@@ -4,6 +4,7 @@ import com.veselov.alex.racecar.data.dao.AutoRepository;
 import com.veselov.alex.racecar.service.parser.CarsAvByParser;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,14 +21,16 @@ public class CarController {
     @GetMapping("/find")
     public String findCars(@RequestParam String query, Model model) {
         log.info("In controller, parsing the query -> {}", query);
-        model.addAttribute("cars", parser.parseSite(query));
+        model.addAttribute("cars", this.parser.parseSite(query));
         return "welcome";
     }
 
     @GetMapping("/cars")
-    public String getCars(Model model) {
+    public String getCars(Model model, @RequestParam(defaultValue = "1") int pageNumber) {
         log.info("Show all cars");
-        model.addAttribute("cars", repository.findAll());
+        final var carsByPage = 10;
+        model.addAttribute("carQuantity", this.repository.count());
+        model.addAttribute("cars", this.repository.findAll(PageRequest.of(pageNumber - 1, carsByPage)).toList());
         return "cars";
     }
 }
