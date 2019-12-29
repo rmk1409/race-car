04752f654bb75d4926b33693872d5f3d3dc62418
Pogorelov Roman <rmk140987@gmail.com>
2019-12-29 13:48:32 +0300

    Add trim binder

--- src/main/java/com/veselov/alex/racecar/controller/QueryController.java
+++ src/main/java/com/veselov/alex/racecar/controller/QueryController.java
@@ -4,13 +4,12 @@ import com.veselov.alex.racecar.data.entity.Query;
 import com.veselov.alex.racecar.service.QueryService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -20,6 +19,15 @@ import java.util.List;
 public class QueryController {
     @Autowired
     private QueryService service;
+
+    /**
+     * It will trim all input strings
+     * @param binder
+     */
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        binder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
+    }
 
     @GetMapping("/queries")
     public String showQueries(Model model) {
