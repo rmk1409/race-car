434712eb05f789774ee2840b7b63c90b9ad74cb0
Pogorelov Roman <rmk140987@gmail.com>
2020-01-11 17:49:17 +0300

    Add Security Configuration

---
+++ src/main/java/com/veselov/alex/racecar/security/SecurityConfiguration.java
@@ -0,0 +1,26 @@
+package com.veselov.alex.racecar.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/h2-console/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin();
+
+        http.csrf()
+                .ignoringAntMatchers("/h2-console/**");
+        http.headers()
+                .frameOptions()
+                .sameOrigin();
+    }
+}
